b1ac721ef02d04453b946ecaeb3b9e51076aeb04
Anisur Rahman <root@Xenos>
2017-05-31 11:21:59 +0600

    some changes on product sell view
     Committer: Anisur Rahman <arovi96@gmail.com>

--- resources/views/productsell.blade.php
+++ resources/views/productsell.blade.php
@@ -1,77 +1,87 @@
 @extends('layouts.app')
-@section('title','Sell Product Here')  
+@section('title','Sell Product Here')
 @section('content')
- <div class="panel-body">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					Entry Product Here
+				</div>
+				<div class="panel-body">
+					<form action="/sell" method="POST" class="form-horizontal">
+						{{ csrf_field() }}
 
-        <!-- New Task Form -->
-        <form action="/sell" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
+						<!-- Task Name -->
+						<div class="form-group">
+							<label for="cmobileName" class="col-sm-3 control-label">Customer Mobile No. :</label>
 
-            <!-- Task Name -->
-            <div class="form-group">
-                <label for="cmobileName" class="col-sm-3 control-label">Customer Mobile No. :</label>
+							<div class="col-sm-6">
+								<input type="text" name="cmobileName" id="task-name" class="form-control">
+							</div>
+						</div>
 
-                <div class="col-sm-6">
-                    <input type="text" name="cmobileName" id="task-name" class="form-control">
-                </div>
-            </div>
-            
-           <div class="form-group">
-                <label for="cname" class="col-sm-3 control-label"> Customer Name :</label>
+						<div class="form-group">
+							<label for="cname" class="col-sm-3 control-label"> Customer Name :</label>
 
-                <div class="col-sm-6">
-                    <input type="text" name="cname" id="task-name" class="form-control">
-                </div>
-            </div>
-            
-            <div class="form-group">
-                <label for="SellProduct" class="col-sm-3 control-label">Brand Name :</label>
+							<div class="col-sm-6">
+								<input type="text" name="cname" id="task-name" class="form-control">
+							</div>
+						</div>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>
-            
-                <div class="form-group">
-                <label for="SellProduct" class="col-sm-3 control-label">Model No. :</label>
+						<div class="form-group">
+							<label for="SellProduct" class="col-sm-3 control-label">Brand Name :</label>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>
-            
-                <div class="form-group">
-                <label for="SellProduct" class="col-sm-3 control-label">Amount :</label>
+							<div class="col-sm-6">
+								<input type="text" name="name" id="task-name" class="form-control">
+							</div>
+						</div>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>
-              
-              <div class="form-group">
-                <label for="SellProduct" class="col-sm-3 control-label">Price of one product :</label>
+						<div class="form-group">
+							<label for="SellProduct" class="col-sm-3 control-label">Model No. :</label>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>
-              
-              <div class="form-group">
-                <label for="SellProduct" class="col-sm-3 control-label">Total price :</label>
+							<div class="col-sm-6">
+								<input type="text" name="name" id="task-name" class="form-control">
+							</div>
+						</div>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>
-            
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Done
-                    </button>
-                </div>
-            </div>
-            
-        </form>
-    </div>
+						<div class="form-group">
+							<label for="SellProduct" class="col-sm-3 control-label">Amount :</label>
+
+							<div class="col-sm-6">
+								<input type="text" name="name" id="task-name" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="SellProduct" class="col-sm-3 control-label">Price of one product :</label>
+
+							<div class="col-sm-6">
+								<input type="text" name="name" id="task-name" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="SellProduct" class="col-sm-3 control-label">Total price :</label>
+
+							<div class="col-sm-6">
+								<input type="text" name="name" id="task-name" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-sm-offset-3 col-sm-6">
+								<button type="submit" class="btn btn-default">
+									<i class="fa fa-plus"></i> Done
+								</button>
+							</div>
+						</div>
+
+					</form>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
 @endsection

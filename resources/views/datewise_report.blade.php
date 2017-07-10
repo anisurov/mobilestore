1ba0357d771ff59eebc2fe74152e84c727b1680f
Anisur Rahman <arovi96@gmail.com>
2017-07-10 15:36:17 +0600

    datepicker added and more

--- resources/views/datewise_report.blade.php
+++ resources/views/datewise_report.blade.php
@@ -1,83 +1,115 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 @section('title','Entry Product Here')
 @section('content')
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />
-<link rel="stylesheet" href="assets/signup-form.css" type="text/css" />
+<link href="{{ asset('bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('jquery-ui/jquery-ui.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/components.css') }}" rel="stylesheet" type="text/css">
+<script src="{{ asset('js/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('js/availProduct.js') }}"></script>
+<script src="{{ asset('jquery-ui/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js') }}"></script>
+<script src="{{ asset('js/metronic.js') }}"></script>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-6">
+			<h1> {{ config('app.name') }} </h1>
+		</div>
+		<div class="col-xs-6 text-right">
+			<h1>INVOICE</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="panel-body">
+				{{ csrf_field() }}
+				<div class="col-md-6">
+					<div class="input-group input-xlarge datetimepicker">
+						<input type="text" name="from_date" id="from_date"  class="form-control text-center" value="" placeholder="From date" />
+						<span class="input-group-addon"> to </span>
+						<input type="text" name="to_date" id="to_date"  class="form-control text-center" value="" placeholder="To date"/>
+					</div>
+				</div>
 
-    <div class="container">
+				<div class="col-md-2">
+					<span class="input-group-btn">
+						<button type="submit" id="submit_form" class="btn blue">
+							<i class="fa fa-search"></i>
+						</button> </span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-6">
+			<div class="panel-body">
+				<div class="form-group">
+					<label for="modelno" class="col-md-4 control-label">Model No.</label>
+					<div class="col-md-6">
+						<select name="modelno" class="form-control" style="width:350px">
+							<option value="">--- Select Model ---</option>
+						</select>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr/>
+	<!-- / end client details section -->
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th><h4>Serial No.</h4></th>
+				<th><h4>Brand Name</h4></th>
+				<th><h4>Model No.</h4></th>
+				<th><h4>Amount</h4></th>
+				<th><h4>Price</h4></th>
+			</tr>
+		</thead>
+		<tbody id="tbody">
 
-    <div class="signup-form-container">
-    
-         <!-- form start -->
-         <form method="post" role="form" id="register-form" autocomplete="off" action="submit.html">
-         
-         <div class="form-header">
-            <h3 class="form-title"><i class="fa fa-user"></i> Sign Up</h3>
-                      
-         <div class="pull-right">
-             <h3 class="form-title"><span class="glyphicon glyphicon-pencil"></span></h3>
-         </div>
-                      
-         </div>
-                  
-         <div class="form-body">
-         
-              <div class="alert alert-info" id="message" style="display:none;">
-              submitted
-              </div>
-                      
-              <div class="form-group">
-                   <div class="input-group">
-                   <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span></div>
-                   <input name="name" type="text" class="form-control" placeholder="Username">
-                   </div>
-                   <span class="help-block" id="error"></span>
-              </div>
-                        
-              <div class="form-group">
-                   <div class="input-group">
-                   <div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></div>
-                   <input name="email" type="text" class="form-control" placeholder="Email">
-                   </div> 
-                   <span class="help-block" id="error"></span>                     
-              </div>
-                        
-              <div class="row">
-                        
-                   <div class="form-group col-lg-6">
-                        <div class="input-group">
-                        <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></div>
-                        <input name="password" id="password" type="password" class="form-control" placeholder="Password">
-                        </div>  
-                        <span class="help-block" id="error"></span>                    
-                   </div>
-                            
-                   <div class="form-group col-lg-6">
-                        <div class="input-group">
-                        <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></div>
-                        <input name="cpassword" type="password" class="form-control" placeholder="Retype Password">
-                        </div>  
-                        <span class="help-block" id="error"></span>                    
-                   </div>
-                            
-             </div>
-                        
-                        
-            </div>
-            
-            <div class="form-footer">
-                 <button type="submit" class="btn btn-info">
-                 <span class="glyphicon glyphicon-log-in"></span> Sign Me Up !
-                 </button>
-            </div>
+		</tbody>
+	</table>
+	<div class="row text-right">
+		<div class="col-xs-2 col-xs-offset-8">
+			<p>
 
+			</p>
+		</div>
+	</div>
+	<hr />
+</div>
+<script type="text/javascript">
+	jQuery("#from_date, #to_date").datepicker({
+		autoclose : true,
+		isRTL : Metronic.isRTL(),
+		format : "yyyy-mm-dd",
+		pickerPosition : (Metronic.isRTL() ? "bottom-right" : "bottom-left")
+	});
+	jQuery("#submit_form").on("click", function() {
 
-            </form>
-            
-           </div>
+		var from_date = jQuery('#from_date').val();
+		var to_date = jQuery('#to_date').val();
 
-    </div>
-    <script src="assets/jquery-1.11.2.min.js"></script>
-    <script src="assets/jquery.validate.min.js"></script>
-    <script src="assets/register.js"></script>
+		if (from_date === '' || to_date === '') {
+
+			if (from_date === '') {
+				jQuery('#from_date').addClass('is_required');
+			} else {
+				jQuery('#from_date').removeClass('is_required');
+			}
+			if (to_date === '') {
+				jQuery('#to_date').addClass('is_required');
+			} else {
+				jQuery('#to_date').removeClass('is_required');
+			}
+			return false;
+
+		} else {
+
+			jQuery("input").removeClass("is_required");
+			return true;
+
+		}
+
+	}); 
+</script>
 @endsection

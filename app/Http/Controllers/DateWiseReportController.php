ded3412a65d364c9dd89304d6f49c67df57a6118
Partha Banik <parthabanik1204@gmail.com>
2017-07-11 21:55:02 +0600

    datereport update

--- app/Http/Controllers/DateWiseReportController.php
+++ app/Http/Controllers/DateWiseReportController.php
@@ -1,50 +1,66 @@
 <?php
 
 namespace mobileStore\Http\Controllers;
+
 use DB;
 use Illuminate\Http\Request;
 
-class DateWiseReportController extends Controller
-{
-    public function __construct(){
+class DateWiseReportController extends Controller {
+
+    public function __construct() {
         $this->middleware('auth');
     }
-	
-	
-	public function index()
-    {
-      return view('datewise_report');
-      
+
+    public function index() {
+        return view('datewise_report');
     }
-     public function report(Request $request)
-    {
-        $fromdata=$request-> input('fromdate');
-        $todate=$request -> input('todate');
-        $data=DB::select('select * from product_entry where date>="'.$fromdata.'" && date<="'.$todate.'"');
+
+    public function report(Request $request) {
+        $fromdata = $request->input('fromdate');
+        $todate = $request->input('todate');
+        $data = DB::select('select * from product_entry where date>="' . $fromdata . '" && date<="' . $todate . '"');
         //$returnData=  array();
-       
-        if($data)
-        {    
-             foreach ($data as $key => $value) {
+          $i=0;
+        if ($data) {
+            foreach ($data as $key => $value) {
+
                 $modelid = $value->model_id;
                 $brandid = $value->brand_id;
-                $brandnames=DB::select('select brandname from brand where brand_id="'.$brandid.'"');
-                   foreach ($brandnames as $key1 => $value1) {
-                $brandname = $value1->brandname;
-            }
-                 $models = DB::select('select model_no from model where brand_id  = "' . $brandid . '" AND model_id ="' . $modelid . '"');
-                  foreach ($models as $key2 => $value2) {
-                $model = $value2->model_no;
-            }
-             
-            $returnData[$key]= array('brandname'=>$brandname,'modelno'=>$model,'amount'=>$value->amount,'buyprice'=>$value->buyprice);
-             
-            }
-       return json_encode($returnData);
-        }
-        else
-        {
+                $brandnames = DB::select('select brandname from brand where brand_id="' . $brandid . '"');
+                foreach ($brandnames as $key1 => $value1) {
+                    $brandname = $value1->brandname;
+                }
+                $models = DB::select('select model_no from model where brand_id  = "' . $brandid . '" AND model_id ="' . $modelid . '"');
+                foreach ($models as $key2 => $value2) {
+                    $model = $value2->model_no;
+                }
+
+                $returnData[$i] = array('transaction' => 'buy', 'brandname' => $brandname, 'modelno' => $model, 'amount' => $value->amount, 'buyprice' => $value->buyprice);
+            $i=$i+1;
+                
+                }
+            $data = DB::select('select * from product_sell where date>="' . $fromdata . '" && date<="' . $todate . '"');
+            foreach ($data as $key => $value) {
+
+                $modelid = $value->model_id;
+                $brandid = $value->brand_id;
+                $brandnames = DB::select('select brandname from brand where brand_id="' . $brandid . '"');
+                foreach ($brandnames as $key1 => $value1) {
+                    $brandname = $value1->brandname;
+                }
+                $models = DB::select('select model_no from model where brand_id  = "' . $brandid . '" AND model_id ="' . $modelid . '"');
+                foreach ($models as $key2 => $value2) {
+                    $model = $value2->model_no;
+                }
+
+                $returnData[$i] = array('transaction' => 'sell', 'brandname' => $brandname, 'modelno' => $model, 'amount' => $value->amount, 'buyprice' => $value->buyprice);
+            $i=$i+1;
+                
+                }
+            return json_encode($returnData);
+        } else {
             return view('home');
         }
     }
+
 }
